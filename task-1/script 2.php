8b7c004e2bad65d568adaa16de3e477634455cb3
drashtichavda <143491700+drashtichavda@users.noreply.github.com>
2023-09-27 11:32:14 +0530

    Add files via upload

---
+++ task-1/script 2.php
@@ -0,0 +1,5 @@
+<?Php
+$name = "drashti";
+$age = 20;
+echo "My name is $name and i am $age years old";
+?>
---
+++ task-1/script 9.php
@@ -0,0 +1,7 @@
+<?php
+$day = 02;
+$month = 04;
+$year = 2003;
+$birthdate = "$day/$month/$year";
+echo $birthdate;
+?
